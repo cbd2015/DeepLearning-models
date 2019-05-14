2d7aef02ef05a6307481fc87d22b7c990b5ce184
cbd2015 <543447223@qq.com>
2019-05-14 11:40:25 +0800

    add Algorith codes

---
+++ convolutionAlgorith/cv_work01/__init__.py
@@ -0,0 +1,39 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+# @Time     : 2019-4-30 11:20
+# @USER     : CHENBD
+# @Site     : midea#2L4
+# @File     : __init__.py
+# @Software : PyCharm
+# @license  : Copyright(C), Midea Company
+# @Author   : chenbd
+# @Email    : 543447223@qq.com
+# @Version  : V1.1.0
+------------------------------------------------- 
+# @Attantion：
+#    1、
+#    2、
+#    3、
+# @Tag      : 
+#
+# @Referne  :
+#    1、
+#    2、
+-------------------------------------------------
+"""
+import cv2 as cv
+import numpy as np
+
+
+def Example():
+    pass
+
+
+if __name__ == "__main__":
+    print ("----Start----")
+
+    Example ()
+
+    print ("----End------")
